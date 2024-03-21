82d50096f6940a29a7168e913cab8677357f37d8
jkress <jonathank1993@gmail.com>
2024-03-21 10:21:23 -0500

    Pedometer Test Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+import { useState, useEffect } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PedometerView />
     </View>
   );
 }
@@ -18,3 +19,46 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const PedometerView = () => {
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+
+  const subscribe = async () => {
+    const isAvailable = await Pedometer.isAvailableAsync();
+    setIsPedometerAvailable(String(isAvailable));
+
+    if (isAvailable) {
+      const end = new Date();
+      const start = new Date();
+      start.setUTCHours(0, 0, 0, 0); // Start of day
+
+      const pastStepCountResult = await Pedometer.getStepCountAsync(start, end);
+      if (pastStepCountResult) {
+        setPastStepCount(pastStepCountResult.steps);
+      }
+
+      return Pedometer.watchStepCount(result => {
+        setCurrentStepCount(result.steps);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const subscription = subscribe();
+    //return () => subscription && subscription.remove();
+  }, []);
+
+  return (
+    <View style={{
+                   flexDirection: 'row',
+                   height: 100,
+                   padding: 20,
+                 }}>
+      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}{"\n"}</Text>
+      <Text>Steps taken today: {pastStepCount}{"\n"}</Text>
+      <Text>Walk! And watch this go up: {currentStepCount}</Text>
+    </View>
+  );
+}
